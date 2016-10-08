c4afc2703a7dfee455e52827a7f43d78403ab4f6
percy <401664018@qq.com>
2016-10-08 16:18:44 +0800

    第一次提交

---
+++ src/com/atguigu/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("要回家啦...");
+		System.out.println("就要回家啦...");
+		System.out.println("马上就要回家啦...");
+		System.out.println("要回家啦...");
+	}
+	
+	
+}
