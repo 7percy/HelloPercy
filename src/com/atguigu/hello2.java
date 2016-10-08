03b8000ba002cace786d19ebabe19a7c3419b35e
percy <401664018@qq.com>
2016-10-08 20:41:33 +0800

    3333

--- src/com/atguigu/hello2.java
+++ src/com/atguigu/hello2.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class hello2 {
   public static void main(String[] args) {
 	System.out.println("1111111111111111");
+	System.out.println("1111111111111111");
+	System.out.println("33333333333");
 }
 }
